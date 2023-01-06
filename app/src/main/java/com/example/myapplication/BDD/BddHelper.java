18b607ef8f0c94c83c799d32f2cbf7186d6196c1
ApDnoze <100912264+ApDnoze@users.noreply.github.com>
2023-01-06 16:20:49 +0100

    BDD
    Base de donnée

---
+++ app/src/main/java/com/example/myapplication/BDD/BddHelper.java
@@ -0,0 +1,24 @@
+package com.example.myapplication.BDD;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class BddHelper extends SQLiteOpenHelper {
+    public BddHelper(@Nullable Context context) { super(context, "fichier.db", null, 1);}
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(LivraisonTable.CREATE_TABLE);
+        db.execSQL(ColisTable.CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(LivraisonTable.CREATE_TABLE);
+        db.execSQL(ColisTable.CREATE_TABLE);
+        onCreate(db);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/BDD/ColisTable.java
@@ -0,0 +1,21 @@
+package com.example.myapplication.BDD;
+
+public class ColisTable {
+    public static final String TABLE_NAME = "colis";
+
+    public static final String COL_REF = "ref";
+    public static final String COL_MONTANT = "montant";
+    public static final String COL_LIVRAISON = "id";
+
+    public static final String CREATE_TABLE = "CREATE TABLE " +
+            TABLE_NAME +
+            " (" +
+            COL_REF +
+            " TEXT PRIMARY KEY," +
+            COL_MONTANT +
+            " INTEGER," +
+            COL_LIVRAISON +
+            " INTEGER);";
+
+    public static final String DROP_TABLE = "DROP TABLE "+ TABLE_NAME;
+}
