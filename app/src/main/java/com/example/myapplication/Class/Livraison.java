7a281c1abd9e603596fc991ae8a4b3714bc0a71c
Martin Chanudet <machanudet@stpbb.org>
2023-01-06 16:38:29 +0100

    update Class get and set + constructor

--- app/src/main/java/com/example/myapplication/Class/Livraison.java
+++ app/src/main/java/com/example/myapplication/Class/Livraison.java
@@ -1,4 +1,46 @@
 package com.example.myapplication.Class;
 
 public class Livraison {
+
+    private int id;
+    private String client;
+    private String adresse;
+
+
+    public Livraison(int id, String client, String adresse) {
+        this.id = id;
+        this.client = client;
+        this.adresse = adresse;
+    }
+    private Livraison(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+
+
+
 }
